20fe7f2c9c3fd838f1c9d27b8e4ac14fdc80eec2
djkool <andrewpeterson86@gmail.com>
2016-02-14 19:00:28 -0500

    Experimentation of a Tiled UI Theme.
    UI is rendered via Tiles to allow for faster rendering of dynamic element
    sizes in a system(pygame) that does do scaled blitting very well.

--- testui.py
+++ testui.py
@@ -3,6 +3,84 @@ from pygame.locals import *
 
 from gamelib.game import GameClass, GameState
 from gamelib import ui
+from gamelib.asset import TileSet
+import paths
+
+
+class TiledTheme(ui.Theme):
+
+    TILE_SIZE = 25
+    MIN_SIZE = TILE_SIZE * 2
+
+    _TILE_FRAME = (
+        24, 25, 26,
+        32, 33, 34,
+        40, 41, 42
+    )
+
+    _TILE_BUTTON = (
+        0, 1, 2,
+        8, 9, 10,
+        16, 17, 18
+    )
+
+    _TILE_DOWN = (
+        3, 4, 5,
+        11, 12, 13,
+        19, 20, 21
+    )
+
+    PADDING = MIN_SIZE
+
+    DEBUG_COLOR = pygame.Color(128, 128, 128)
+
+    def __init__(self, tileset):
+        self.tileset = tileset
+        self.tileset.resize((self.TILE_SIZE, self.TILE_SIZE))
+
+    def drawFrame(self, surf, rect):
+        self._fillRect(surf, rect, self._TILE_FRAME)
+
+    def drawButton(self, surf, rect, state):
+        self._fillRect(surf, rect, self._TILE_DOWN if state == ui.Button.DOWN else self._TILE_BUTTON)
+
+    def _toTileSpace(self, src_rect):
+        halfTile = self.TILE_SIZE/2
+        return pygame.Rect(src_rect.left/self.TILE_SIZE, src_rect.top/self.TILE_SIZE,
+                           (src_rect.width+halfTile)/self.TILE_SIZE, (src_rect.height+halfTile)/self.TILE_SIZE)
+
+    def _drawTile(self, surf, tile_pos, idx):
+        pos = (tile_pos[0]*self.TILE_SIZE, tile_pos[1]*self.TILE_SIZE)
+        self.tileset.render(surf, pos, idx)
+
+    def _fillRect(self, surf, rect, tilemap):
+        trect = self._toTileSpace(rect)
+        irect = trect.inflate(-2, -2)
+
+        # Draw Corners
+        self._drawTile(surf, trect.topleft, tilemap[0])
+        self._drawTile(surf, (irect.right, trect.top), tilemap[2])
+        self._drawTile(surf, irect.bottomright, tilemap[8])
+        self._drawTile(surf, (trect.left, irect.bottom), tilemap[6])
+
+        # Draw Sides
+        if irect.width > 0:
+            for x in xrange(irect.left, irect.right):
+                self._drawTile(surf, (x, trect.top), tilemap[1])
+                self._drawTile(surf, (x, irect.bottom), tilemap[7])
+        if irect.height > 0:
+            for y in xrange(irect.top, irect.bottom):
+                self._drawTile(surf, (trect.left, y), tilemap[3])
+                self._drawTile(surf, (irect.right, y), tilemap[5])
+
+                # Fill middle
+                for x in xrange(irect.left, irect.right):
+                    self._drawTile(surf, (x, y), tilemap[4])
+
+        # DEBUG GRID
+        #pygame.draw.rect(surf, self.DEBUG_COLOR, rect, 1)
+
+#end TiledTheme
 
 
 class TestUI(GameClass):
@@ -28,6 +106,9 @@ class TestUI(GameClass):
         pygame.init()
         self.screen = pygame.display.set_mode(self.SCREEN_SIZE)
         self.clock = pygame.time.Clock()
+
+        ui_tiles = TileSet(paths.getImagePath("rpggame-ui.png"), (16, 16))
+        ui.setTheme(TiledTheme(ui_tiles))
 
     def update(self):
         # Must be done before GameClass.update() so it can
@@ -60,12 +141,13 @@ class MenuState(GameState):
     def initialize(self):
         """Called the first time the game is changed to this state
            during the applications lifecycle."""
-        self.testFrame = ui.Frame(pygame.Rect(180, 125, 300, 200))
+        self.testFrame = ui.Frame(pygame.Rect(175, 125, 300, 200))
         self.testFrame.addChild(ui.Text(pygame.Rect(0, 0, 300, 25), "Test Frame", True))
-        self.testFrame.addChild(ui.EditBox(pygame.Rect(25, 50, 250, 25), "insert text"))
-        self.testFrame.addChild(ui.CheckBox(pygame.Rect(25, 100, 100, 25), "Unchecked", self.checked))
-        self.testFrame.addChild(ui.Button(pygame.Rect(25, 150, 100, 25), "OK", self.buttonNotify))
-        self.testFrame.addChild(ui.Button(pygame.Rect(175, 150, 100, 25), "Cancel", self.buttonNotify))
+        self.testFrame.addChild(ui.EditBox(pygame.Rect(25, 25, 250, TiledTheme.MIN_SIZE), "insert text"))
+        self.testFrame.addChild(ui.CheckBox(pygame.Rect(25, 75, 100, TiledTheme.MIN_SIZE), "Unchecked", self.checked))
+        self.testFrame.addChild(ui.Button(pygame.Rect(25, 125, 100, TiledTheme.MIN_SIZE), "OK", self.buttonNotify))
+        self.testFrame.addChild(ui.Button(pygame.Rect(175, 125, 100, TiledTheme.MIN_SIZE), "Cancel", self.buttonNotify))
+
 
     def enter(self):
         """Called every time the game is switched to this state."""
